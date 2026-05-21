a98a8cf4e260b1745c03c8b92a6e0d14968815ea
nandni <vashikapathak@Mac.lan>
2026-05-22 00:34:45 +0530

    Redirect home route to listings

--- app.js
+++ app.js
@@ -99,6 +99,10 @@ app.use((req,res,next) =>{
 //     res.send(registeredUser);
 // });
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
